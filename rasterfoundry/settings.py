beba57ce4fc192a2bc4d2d94a9a7edf7a0b86596
Lewis Fishgold <lewfish@gmail.com>
2017-10-18 17:49:17 -0400

    Update path to handle new bucket layout

--- rasterfoundry/settings.py
+++ rasterfoundry/settings.py
@@ -1,4 +1,4 @@
 RV_CPU_QUEUE = 'raster-vision-cpu'
 RV_CPU_JOB_DEF = 'raster-vision-cpu'
-RV_CONFIG_URI_ROOT = 's3://raster-vision/datasets/detection/configs'
+RV_PROJ_CONFIG_DIR_URI = 's3://raster-vision-od/configs/projects'
 DEVELOP_BRANCH = 'develop'
